7349b3f1ded00f02e5b49a2dd9a021ee250b6697
Anton Mosharov <anton.mosharov@gmail.com>
2025-05-13 17:04:18 -0300

    ~updated STORAGE_PATH

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import gradio as gr
 from utils import format_article, load_articles
 
 STORAGE_NAME = "arxiv"
-STORAGE_PATH = f"/tmp/{STORAGE_NAME}"
+STORAGE_PATH = f"/var/tmp/{STORAGE_NAME}"
 MAX_ARTICLES = 10
 
 
